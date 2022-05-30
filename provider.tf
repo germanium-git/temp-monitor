b3c73a55454ce268b19b8d733388f4def4546e0d
germanium-git <germanium-git@gmx.com>
2022-05-30 22:13:35 +0200

    provider definition env variables

--- provider.tf
+++ provider.tf
@@ -9,4 +9,8 @@ terraform {
 
 provider "oci" {
    region = "eu-frankfurt-1"
+   tenancy_ocid = var.tenancy_ocid
+   user_ocid = var.user_ocid
+   fingerprint = var.fingerprint
+   private_key = var.private_key
 }
