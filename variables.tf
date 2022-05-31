5895b537086cc090d9d2eff5f89d1ae68bd35bf3
germanium-git <germanium-git@gmx.com>
2022-05-31 16:14:52 +0200

    unused variable pre-auth removed

--- variables.tf
+++ variables.tf
@@ -10,12 +10,6 @@ variable "FINGERPRINT" {
 variable "PRIVATE_KEY" {
   type = string
 }
-
 variable "COMPARTMENT_OCID" {
-  type = string
-}
-
-# Pre-Authenticated Requests to talk to the backend on object storage in Oracle cloud
-variable "PREAUTH_REQUEST" {
   type = string
 }
