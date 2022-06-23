42200aa528cffd84abd9664a9a2dd872712b3bf8
germanium-git <germanium-git@gmx.com>
2022-06-23 14:20:54 +0200

    display name 100GB

--- storage.tf
+++ storage.tf
@@ -1,9 +1,10 @@
-resource "oci_core_volume" "volume_200" {
+resource "oci_core_volume" "volume_100" {
     #Required
     compartment_id = var.COMPARTMENT_OCID
 
     #Optional
     availability_domain = "okdB:EU-FRANKFURT-1-AD-2"
+    display_name = "volume_100GB"
 
     size_in_gbs = 100
 }
@@ -12,6 +13,6 @@ resource "oci_core_volume_attachment" "test_volume_attachment" {
     #Required
     attachment_type = "paravirtualized"
     instance_id = oci_core_instance.apollo.id
-    volume_id = oci_core_volume.volume_200.id
+    volume_id = oci_core_volume.volume_100.id
 
 }
