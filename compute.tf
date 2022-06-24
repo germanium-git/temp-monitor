4bc5f62220578a68bb259de4efbe0e96cc565fb8
germanium-git <germanium-git@gmx.com>
2022-06-24 10:10:04 +0200

    Canonical-Ubuntu-22.04-aarch64-2022.05.17-0

--- compute.tf
+++ compute.tf
@@ -124,6 +124,7 @@ output "public-ip-apollo" {
 # ocid1.image.oc1.eu-frankfurt-1.aaaaaaaaxp2zqotv37r4zycmwfqywcujsh4scenphjjs5w2ozakmidg3vs6q
 
 
+/*
 
 resource "oci_core_instance" "apollo" {
   # Required
@@ -135,6 +136,52 @@ resource "oci_core_instance" "apollo" {
 
   source_details {
     source_id   = "ocid1.image.oc1.eu-frankfurt-1.aaaaaaaaxp2zqotv37r4zycmwfqywcujsh4scenphjjs5w2ozakmidg3vs6q"
+    source_type = "image"
+  }
+
+  # Optional
+  display_name = "apollo"
+
+  shape_config {
+    ocpus         = 1
+    memory_in_gbs = 6
+  }
+
+  create_vnic_details {
+    assign_public_ip = true
+    subnet_id        = oci_core_subnet.influx_subnet_01.id
+    nsg_ids          = [oci_core_network_security_group.influx_nsg.id]
+  }
+
+  metadata = {
+    ssh_authorized_keys = file("sshkey/oracle-cloud_2021-09-08_id_rsa.pub")
+  }
+
+  preserve_boot_volume = false
+}
+
+
+# Outputs for compute instance
+output "public-ip-apollo" {
+  value = oci_core_instance.apollo.public_ip
+}
+
+*/
+
+# Canonical-Ubuntu-22.04-aarch64-2022.05.17-0
+# ocid1.image.oc1.eu-frankfurt-1.aaaaaaaa7eis5rnrr2tyvwa27ko53yp4ua7jur7xgnhppockzytlsa3soara
+
+
+resource "oci_core_instance" "apollo" {
+  # Required
+  #availability_domain = data.oci_identity_availability_domains.ads.availability_domains[0].name
+  availability_domain = "okdB:EU-FRANKFURT-1-AD-2"
+  compartment_id      = var.COMPARTMENT_OCID
+  #shape              = "VM.Standard.E2.1.Micro"
+  shape               = "VM.Standard.A1.Flex"
+
+  source_details {
+    source_id   = "ocid1.image.oc1.eu-frankfurt-1.aaaaaaaa7eis5rnrr2tyvwa27ko53yp4ua7jur7xgnhppockzytlsa3soara"
     source_type = "image"
   }
 
