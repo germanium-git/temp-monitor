93c2224c633cd7e20ffe8a62ed431fb590734a77
germanium-git <germanium-git@gmx.com>
2022-07-18 10:52:35 +0200

    nsg rules TE

--- variables.tf
+++ variables.tf
@@ -30,6 +30,8 @@ variable "nsg_rules" {
   # Options are supported only for ICMP ("1"), TCP ("6"), UDP ("17"), and ICMPv6 ("58").
   default = {
     SSH   = ["SSH from my public IP", "185.230.172.74/32", "6", "22"]
+    SSH_TE   = ["SSH from TE", "131.207.0.0/16", "6", "22"]
     influx   = ["InfluxDB my public IP", "185.230.172.74/32", "6", "8086"]
+    influx_TE   = ["InfluxDB from TE", "131.207.0.0/16", "6", "8086"]
   }
 }
