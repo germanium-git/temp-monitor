2f40e0b85c873f16d3fbf60c064edf2d8a844c57
germanium-git <germanium-git@gmx.com>
2022-07-22 10:32:49 +0200

    public ip reference fixed

--- cloudflare.tf
+++ cloudflare.tf
@@ -1,8 +1,7 @@
 # Create A record in Cloudflare DNS for the compute node
-resource "cloudflare_record" "root" {
+resource "cloudflare_record" "apollo" {
   zone_id = var.cloudflare_zone_id
   name    = "apollo"
-  value   = public-ip-apollo
+  value   = oci_core_instance.apollo.public_ip
   type    = "A"
 }
-
