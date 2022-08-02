a1146219dad746412f43c48ff7bee9ac7b885c54
germanium-git <germanium-git@gmx.com>
2022-08-02 12:36:02 +0200

    staging

--- provider.tf
+++ provider.tf
@@ -38,9 +38,9 @@ provider "cloudflare" {
 
 provider "acme" {
   # Staging
-  # server_url = "https://acme-staging-v02.api.letsencrypt.org/directory"
+  server_url = "https://acme-staging-v02.api.letsencrypt.org/directory"
   
   # Production
-  server_url = "https://acme-v02.api.letsencrypt.org/directory"
+  #server_url = "https://acme-v02.api.letsencrypt.org/directory"
   
 }
